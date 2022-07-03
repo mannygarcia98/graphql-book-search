fae0d49792cb1292a06da5f6ed15a0af04585b13
mannygarcia98 <mannysg88@gmail.com>
2022-07-03 16:42:42 -0500

    add mutations

---
+++ client/src/utils/queries.js
@@ -0,0 +1,17 @@
+export const GET_ME = gql`
+  query me {
+    me {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        description
+        image
+        link
+        title
+      }
+    }
+  }
+`;
